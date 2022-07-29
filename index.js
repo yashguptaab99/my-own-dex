4596660e049a764f8123e14cdbd8202b3ec0b83d
Yash Gupta <yashguptaab99@gmail.com>
2022-07-30 04:33:31 +0530

    feat: Set a token allowance

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
+const BigNumber = require('bignumber.js');
 const qs = require('qs');
+const web3 = require('web3');
 
 let currentTrade = {};
 let currentSelectSide;
@@ -23,9 +25,9 @@ async function listAvailableTokens() {
     let div = document.createElement('div');
     div.className = 'token_row';
     let html = `
-    <img class="token_list_img" src="${tokens[i].logoURI}">
-      <span class="token_list_text">${tokens[i].symbol}</span>
-      `;
+        <img class="token_list_img" src="${tokens[i].logoURI}">
+          <span class="token_list_text">${tokens[i].symbol}</span>
+          `;
     div.innerHTML = html;
     div.onclick = () => {
       selectToken(tokens[i]);
@@ -37,7 +39,7 @@ async function listAvailableTokens() {
 async function selectToken(token) {
   closeModal();
   currentTrade[currentSelectSide] = token;
-  console.log('currentTrade:', currentTrade);
+  console.log('currentTrade: ', currentTrade);
   renderInterface();
 }
 
@@ -49,6 +51,7 @@ function renderInterface() {
       currentTrade.from.symbol;
   }
   if (currentTrade.to) {
+    console.log(currentTrade.to);
     document.getElementById('to_token_img').src = currentTrade.to.logoURI;
     document.getElementById('to_token_text').innerHTML = currentTrade.to.symbol;
   }
@@ -135,7 +138,7 @@ async function getQuote(account) {
     sellToken: currentTrade.from.address,
     buyToken: currentTrade.to.address,
     sellAmount: amount,
-    takerAddress: account,
+    // takerAddress: account,
   };
 
   // Fetch the swap quote.
@@ -154,6 +157,206 @@ async function getQuote(account) {
   return swapQuoteJSON;
 }
 
+async function trySwap() {
+  const erc20abi = [
+    {
+      inputs: [
+        { internalType: 'string', name: 'name', type: 'string' },
+        { internalType: 'string', name: 'symbol', type: 'string' },
+        { internalType: 'uint256', name: 'max_supply', type: 'uint256' },
+      ],
+      stateMutability: 'nonpayable',
+      type: 'constructor',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'owner',
+          type: 'address',
+        },
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'spender',
+          type: 'address',
+        },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'Approval',
+      type: 'event',
+    },
+    {
+      anonymous: false,
+      inputs: [
+        {
+          indexed: true,
+          internalType: 'address',
+          name: 'from',
+          type: 'address',
+        },
+        { indexed: true, internalType: 'address', name: 'to', type: 'address' },
+        {
+          indexed: false,
+          internalType: 'uint256',
+          name: 'value',
+          type: 'uint256',
+        },
+      ],
+      name: 'Transfer',
+      type: 'event',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'owner', type: 'address' },
+        { internalType: 'address', name: 'spender', type: 'address' },
+      ],
+      name: 'allowance',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'spender', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'approve',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [{ internalType: 'address', name: 'account', type: 'address' }],
+      name: 'balanceOf',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [{ internalType: 'uint256', name: 'amount', type: 'uint256' }],
+      name: 'burn',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'account', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'burnFrom',
+      outputs: [],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'decimals',
+      outputs: [{ internalType: 'uint8', name: '', type: 'uint8' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'spender', type: 'address' },
+        { internalType: 'uint256', name: 'subtractedValue', type: 'uint256' },
+      ],
+      name: 'decreaseAllowance',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'spender', type: 'address' },
+        { internalType: 'uint256', name: 'addedValue', type: 'uint256' },
+      ],
+      name: 'increaseAllowance',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'name',
+      outputs: [{ internalType: 'string', name: '', type: 'string' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'symbol',
+      outputs: [{ internalType: 'string', name: '', type: 'string' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [],
+      name: 'totalSupply',
+      outputs: [{ internalType: 'uint256', name: '', type: 'uint256' }],
+      stateMutability: 'view',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'recipient', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'transfer',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+    {
+      inputs: [
+        { internalType: 'address', name: 'sender', type: 'address' },
+        { internalType: 'address', name: 'recipient', type: 'address' },
+        { internalType: 'uint256', name: 'amount', type: 'uint256' },
+      ],
+      name: 'transferFrom',
+      outputs: [{ internalType: 'bool', name: '', type: 'bool' }],
+      stateMutability: 'nonpayable',
+      type: 'function',
+    },
+  ];
+  console.log('trying swap');
+
+  // Only work if MetaMask is connect
+  // Connecting to Ethereum: Metamask
+  const web3 = new Web3(Web3.givenProvider);
+
+  // The address, if any, of the most recently used account that the caller is permitted to access
+  let accounts = await ethereum.request({ method: 'eth_accounts' });
+  let takerAddress = accounts[0];
+  console.log('takerAddress: ', takerAddress);
+
+  const swapQuoteJSON = await getQuote(takerAddress);
+
+  // Set Token Allowance
+  // Set up approval amount
+  const fromTokenAddress = currentTrade.from.address;
+  const maxApproval = new BigNumber(2).pow(256).minus(1);
+  console.log('approval amount: ', maxApproval);
+  const ERC20TokenContract = new web3.eth.Contract(erc20abi, fromTokenAddress);
+  console.log('setup ERC20TokenContract: ', ERC20TokenContract);
+
+  // Grant the allowance target an allowance to spend our tokens.
+  const tx = await ERC20TokenContract.methods
+    .approve(swapQuoteJSON.allowanceTarget, maxApproval)
+    .send({ from: takerAddress })
+    .then((tx) => {
+      console.log('tx: ', tx);
+    });
+}
+
 init();
 
 document.getElementById('login_button').onclick = connect;
@@ -165,3 +368,4 @@ document.getElementById('to_token_select').onclick = () => {
 };
 document.getElementById('modal_close').onclick = closeModal;
 document.getElementById('from_amount').onblur = getPrice;
+document.getElementById('swap_button').onclick = trySwap;
